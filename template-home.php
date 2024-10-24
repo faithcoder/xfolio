9824e607271e9623f800fca3b6aae1007f2f51a4
Abdullah Al Arif <48954386+faithcoder@users.noreply.github.com>
2024-10-24 23:35:53 +0600

    home page design

---
+++ template-home.php
@@ -0,0 +1,25 @@
+<?php
+/*
+ * Template Name: Home
+ */
+
+get_header(); // This will include the default header
+?>
+
+<main class="main-wrapper">
+    <div class="welcome-text">
+        <h3>hi it's</h3>
+        <h1>emran</h1>
+        <h4>Product Designer</h4>
+    </div>
+    <div class="welcome-description">
+        <p>Designing solutions in various types of industries for more than 5 years. I focused on clear visual concepts that create an impactful experience and meet the business goal for users and product owners. I believe, beauty is just an expression but In the end, we pay for the solution that makes us feel better and safe.</p>
+    </div>
+
+    <div class="contact-mail">
+        <p><a href="">contact@hiemran.com</a></p>
+    </div>
+</main><!-- .main-wrapper -->
+
+<?php
+// No footer is included here, as requested
