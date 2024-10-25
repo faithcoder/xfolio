212898d3ca432a53d41b217f5ceb874dda5a6bcf
Abdullah Al Arif <48954386+faithcoder@users.noreply.github.com>
2024-10-25 21:26:21 +0600

    styles are updated

---
+++ template-about.php
@@ -0,0 +1,14 @@
+<?php
+/*
+ * Template Name: About
+ */
+
+get_header(); // This will include the default header
+?>
+
+<main class="main-wrapper">
+    
+</main><!-- .main-wrapper -->
+
+<?php
+// No footer is included here, as requested
